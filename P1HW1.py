d82be36206dd698f4c684546c7a0a50d3dd0ec7e
JamesMcNeil1212 <mcneilj1119@student.faytechcc.edu>
2024-09-04 18:33:55 +0000

    add into file

---
+++ P1HW1.py
@@ -0,0 +1,22 @@
+# James McNeil
+# 9/4/2024
+# P1HW1
+# Input and Output with Integers
+
+print("----Calculating Exponents----")
+print()
+print()
+base_value=int(input("Enter an integer as the base value: "))
+exponent = int(input("Enter an integer as the exponent: "))
+print()
+print()
+print(base_value, "raise to the power of", exponent, "is", base_value**exponent, "!!")
+print()
+print()
+print("----Addition and Substraction----")
+my_num = int(input("Enter a starting integer: "))
+add = int(input("Enter an integer to add: "))
+subtract = int(input("Enter an integer to subtract: "))
+print()
+print(my_num, "+", add, "-", subtract, "is equal to", my_num + add - subtract)2
+2
