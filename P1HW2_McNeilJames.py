241aad51f3f2cb3f911a831e218477d6a3a2832f
JamesMcNeil1212 <mcneilj1119@student.faytechcc.edu>
2024-09-07 15:44:57 -0400

    Create P1HW2_McNeilJames.py

---
+++ P1HW2_McNeilJames.py
@@ -0,0 +1,17 @@
+mmmMmmmMMMm# James McNeil
+# 9/7/24
+# P1HW2
+# Budgeting Traveling Expenses
+print("This program calculates and displays travel expenses")
+print()
+budget=int(input("Enter Budget: "))
+print()
+travel_destination=int(input("Enter your travel destination: "))
+print()
+gas_money=int(input("How much do you think you'll spend on gas? "))
+print()
+living_fee=int(input("Approximately, how much will you need for accommodation/hotel? "))
+print()
+food=int(input("Last, how much do you need for food? "))
+print()
+------------Travel Expenses------------
